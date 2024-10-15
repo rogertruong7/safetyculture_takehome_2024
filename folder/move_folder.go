ea458c69743fcc65bc0acca4a16542e9431c21c3
Roger <z5477692@ad.unsw.edu.au>
2024-10-16 01:23:23 +1100

    bug fixing

--- folder/move_folder.go
+++ folder/move_folder.go
@@ -15,7 +15,6 @@ func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
 	}
 	
 	sourceExists := false
-	sourcePath := ""
 	sourceOrgId := uuid.Nil
 	dstExists := false
 	dstPath := ""
@@ -25,7 +24,6 @@ func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
 		if f.Name == name {
 			sourceExists = true
 			sourceOrgId = f.OrgId
-			sourcePath = f.Paths
 		} else if f.Name == dst {
 			dstExists = true
 			dstOrgId = f.OrgId
@@ -43,8 +41,8 @@ func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
 	// Checking if dst is a child of source
 	dstIndex := -1
 	sourceIndex := -1
-	sourcePathParts := strings.Split(sourcePath, ".")
-	for i, folder := range sourcePathParts {
+	dstPathParts := strings.Split(dstPath, ".")
+	for i, folder := range dstPathParts {
 		if folder == dst {
 			dstIndex = i
 		} else if folder == name {
@@ -52,7 +50,7 @@ func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
 		}
 	}
 
-	if dstIndex != -1 {
+	if sourceIndex != -1 {
 		if (sourceIndex < dstIndex) {
 			return nil, fmt.Errorf("error: cannot move a folder to a child of itself")
 		}
@@ -63,6 +61,7 @@ func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
     dstPath = fmt.Sprintf(`%s.`, dstPath)
     for _, f := range folders { 
         if f.OrgId == sourceOrgId && strings.Contains(f.Paths, name) {
+			// this ensures that we are not finding a path that just contains the name of the folder
 			parts := strings.Split(f.Paths, ".")
 			nameIndex := -1
 			for index, part := range parts {
@@ -71,17 +70,15 @@ func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
 				}
 			}
 			if nameIndex == -1 {
-				// We know that source folder does exist from above. This ensures that we are not finding a path that just contains the name of the folder e.g. we want abc, but abcd also contains abc
 				res = append(res, f)
 				continue
 			}
 			combinedString := strings.Join(parts[nameIndex:], ".")
-            newFolder := Folder{
-				Name: f.Name,
-				OrgId: f.OrgId,
-				Paths: fmt.Sprintf(`%s%s`, dstPath, combinedString),
-			}
-            res = append(res, newFolder)
+            res = append(res, Folder{
+					Name: f.Name,
+					OrgId: f.OrgId,
+					Paths: fmt.Sprintf(`%s%s`, dstPath, combinedString),
+				})
         } else {
             res = append(res, f)
         }
